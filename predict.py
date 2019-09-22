5351d43c95de160a98373afb5f9a03296338c2d9
arpanjhawar <47554411+arpanjhawar@users.noreply.github.com>
2019-09-21 22:21:15 -0700

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,27 +1,52 @@
-import cv2
+import numpy as np
 import os
-import tensorflow as tf
-from keras import Sequential
+from matplotlib import pyplot as plt
+import cv2
+import random
+import pickle
 
-
+file_list = []
+class_list = []
+data_path = "chest_xray/chest_xray/train"
+# All the categories you want your neural network to detect
 cat = ["NORMAL", "PNEUMONIA"]
-
-
-def prepare(file):
-    imsz = 200
-    img_array = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
-    new_array = cv2.resize(img_array, (imsz, imsz))
-    return new_array.reshape(-1, imsz, imsz, 1)
-
-
-model = tf.keras.models.load_model("CNN4.model")
-data_path = "chest_xray/chest_xray/test"
+imgsize = 200
 for a in cat:
-    print("Hello world")
     path = os.path.join(data_path, a)
     for img in os.listdir(path):
-        iarr = os.path.join(path, img)
-        image = prepare(iarr)
-        prediction = model.predict([image])
-        prediction = list(prediction[0])
-        print(cat[prediction.index(max(prediction))])
+        iarr = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+training_data = []
+print("Hello World 2")
+
+
+def create_training_data():
+    for category in cat:
+        pa = os.path.join(data_path, category)
+        class_num = cat.index(category)
+        for imag in os.listdir(pa):
+            img_array = cv2.imread(os.path.join(pa, imag), cv2.IMREAD_GRAYSCALE)
+            new_array = cv2.resize(img_array, (imgsize, imgsize))
+            training_data.append([new_array, class_num])
+
+
+print("Hello World 3")
+create_training_data()
+print("Hello World")
+random.shuffle(training_data)
+x = []
+y = []
+for f, l in training_data:
+    x.append(f)
+    y.append(l)
+x = np.array(x).reshape(-1, imgsize, imgsize, 1)
+pickle_out = open("X.pickle", "wb")
+pickle.dump(x, pickle_out)
+pickle_out.close()
+print("Hello World 4")
+pickle_out = open("y.pickle", "wb")
+pickle.dump(y, pickle_out)
+pickle_out.close()
+print("Hello World 5")
+pickle_in = open("X.pickle", "rb")
+x = pickle.load(pickle_in)
+print("Hello World 6")
